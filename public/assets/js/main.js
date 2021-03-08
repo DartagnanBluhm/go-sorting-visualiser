9c46d011c78eaea5b4155b2a40f78db132b9109f
Dartagnan <tadartagnan@hotmail.com>
2021-03-08 19:32:07 +1030

    finished implementation and styling of body controls

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,12 +1,18 @@
+function shuffleBars(){
+    populateVisualisation(document.getElementById("sorting-bar-number").value)
+}
+
+function displayValue(){
+    document.getElementById("sort-speed-label").innerHTML = "action speed (time(" + document.getElementById("sort-speed").value + "ms) between actions)"
+}
+
 function populateVisualisation(length) {
     container = document.getElementById("bar-container")
     width = parseInt(window.getComputedStyle(container, null).getPropertyValue('width'))
     height = parseInt(window.getComputedStyle(container, null).getPropertyValue('height'))
     if (length > width/2) {
         length = Math.floor(width/2);
-        console.log(length)
     }
-    console.log(width, height)
     container.innerHTML = ""
     for (i = 0; i < length; i++) {
         value = Math.floor(Math.random() * height + 1)
